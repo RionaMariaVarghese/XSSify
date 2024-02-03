4e69cf95d17300e232fa82215ff490d393d1d998
RionaMariaVarghese <rionamariav@gmail.com>
2024-02-03 11:32:37 +0530

    Added comments

--- main.py
+++ main.py
@@ -12,33 +12,38 @@ class MainWindow(QMainWindow):
         welcome_page = WelcomePage()
         self.setCentralWidget(welcome_page)
 
-        self.setWindowTitle("My App")
+        # App window title and icon
+        self.setWindowTitle("XSSify")
+
+        # Setting default font
         app_font = QFont("JetBrains Mono")
         QApplication.instance().setFont(app_font)
 
+        # Setting main bg color
         main_window_color = QColor("#252839")
         self.setAutoFillBackground(True)
         p = self.palette()
         p.setColor(self.backgroundRole(), main_window_color)
         self.setPalette(p)
 
+        # Timer for welcome page
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.show_index_page)
         self.timer.start(2500)
 
+    def show_index_page(self):
+        self.timer.stop()
+        index_page = IndexPage()
+        self.setCentralWidget(index_page)
+
+    # Centering contents
     def center_window(self):
         screen_geo = QApplication.primaryScreen().geometry()
         x = (screen_geo.width() - self.width()) // 2
         y = (screen_geo.height() - self.height()) // 2
         self.move(x, y)
 
-    def show_index_page(self):
-        # Stop the timer to prevent it from triggering again
-        self.timer.stop()
-        # Make IndexPage the centralWidget
-        index_page = IndexPage()
-        self.setCentralWidget(index_page)
-
+# Main function
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MainWindow()
