b714655e0f05a0b2ee32ab46bc1f12bb92afcf81
RionaMariaVarghese <rionamariav@gmail.com>
2024-05-11 14:31:43 +0530

    Levels content added to sqlite database.

--- src/level_content.py
+++ src/level_content.py
@@ -27,6 +27,82 @@ content_entries = [
         question='Which programming language is commonly used for XSS payloads?',
         answer='JavaScript',
         requires_input=1
+    ),
+    LevelPageContent(
+        question='Code sanitization converts the symbol, "<" to?',
+        answer='&lt;',
+        requires_input=1
+    ),
+    LevelPageContent(
+        question='Full form of DOM?',
+        answer='Document Object Model',
+        requires_input=1
+    ),
+    LevelPageContent(
+        question='Which of the following is NOT a common technique to prevent XSS attacks?',
+        option1='Session management',
+        option2='Input validation"',
+        option3='Output encoding',
+        option4='Content Security Policy (CSP)',
+        answer='Session management',
+        requires_input=0
+    ),
+    LevelPageContent(
+        question='What does XSS stand for?',
+        answer='Cross Site Scripting',
+        requires_input=1
+    ),
+    LevelPageContent(
+        question='Which type of XSS attack occurs when user input is reflected immediately without any sanitization?',
+        option1='Stored XSS',
+        option2='DOM-based XSS',
+        option3='Reflected XSS',
+        option4='Persistent XSS',
+        answer='Reflected XSS',
+        requires_input=0
+    ),
+    LevelPageContent(
+        question='What does CSP stand for?',
+        answer='Content Security Policy',
+        requires_input=1
+    ),
+    LevelPageContent(
+        question='Write a script that can pop up an alert message "1".',
+        answer='<script>alert("1")</script>',
+        requires_input=1
+    ),
+    LevelPageContent(
+        question='Which HTML tag is commonly used to sanitize user input and prevent XSS attacks?',
+        option1='<input>',
+        option2='<div>',
+        option3='<script>',
+        option4='<textarea>',
+        answer='<textarea>',
+        requires_input=0
+    ),
+    LevelPageContent(
+        question='In e-commerce websites, where are XSS vulnerabilities often found?',
+        answer='Injection',
+        requires_input=1
+    ),
+    LevelPageContent(
+        question='Number of main types of XSS?',
+        answer='3',
+        requires_input=1
+    ),
+    LevelPageContent(
+        question='Which type of XSS attack occurs when the attacker can manipulate client-side scripts to execute malicious code in the victim\'s browser?',
+        option1='Reflected XSS',
+        option2='DOM-based XSS',
+        option3='Stored XSS',
+        option4='Server-side XSS',
+        answer='DOM-based XSS',
+        requires_input=0
+    ),
+    LevelPageContent(
+        question='What field is ususally targeted for XSS attack? (Input/Image/Database)',
+        answer='input',
+        requires_input=1
     )
 ]
 
